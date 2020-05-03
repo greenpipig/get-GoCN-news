cafc5368dbef8c17a8c853206eddd58010c35663
绿皮豚 <51350819+greenpipig@users.noreply.github.com>
2020-05-03 13:38:22 +0800

    Update update.sh

--- update.sh
+++ update.sh
@@ -6,8 +6,8 @@ git push origin master
 # shellcheck disable=SC2164
 cd GoCN-news
 timeTitle=$time$title
-cp $timeTitle ~/myblog/source/_posts/
+cp $timeTitle ~/myblog/source/_posts/#将生成的md文件放置到hexo目录下，此处要修改路径名
 # shellcheck disable=SC2164
-cd ~/myblog #进入自己的blog地址
+cd ~/myblog #进入自己的blog地址，此处要修改路径名
 hexo clean
 hexo d
