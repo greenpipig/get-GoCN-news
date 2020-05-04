d9103678d4ee36f636495472c5c018df31fe9dc3
xingyuping <xingyuping@bytedance.com>
2020-05-04 15:45:18 +0800

    fix(sh):fix update.sh
    Change-Id: Icb46b79fe8148d982b0a58fdaa9296cc2b2924eb

--- update.sh
+++ update.sh
@@ -6,7 +6,7 @@ git push origin master
 # shellcheck disable=SC2164
 cd GoCN-news
 timeTitle=$time$title
-cp $timeTitle ~/myblog/source/_posts/#将生成的md文件放置到hexo目录下，此处要修改路径名
+cp $timeTitle ~/myblog/source/_posts/ #将生成的md文件放置到hexo目录下，此处要修改路径名
 # shellcheck disable=SC2164
 cd ~/myblog #进入自己的blog地址，此处要修改路径名
 hexo clean
